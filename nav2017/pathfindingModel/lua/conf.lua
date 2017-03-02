3e8eeed103ad6092014fe703ff35c170e2d98523
fenceFoil <karnavasbj@gmail.com>
2017-03-02 18:04:42 -0500

    Changed: Renamed window title from NAVTune to Navigation

--- nav2017/pathfindingModel/lua/conf.lua
+++ nav2017/pathfindingModel/lua/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
-  t.title = "NAVTune Visualizer - UNCC_ASTR1 - NASA RMC 2017"
-  t.window.title = "NAVTune Visualizer - UNCC_ASTR1 - NASA RMC 2017"
+  t.title = "Navigation Visualizer - UNCC_ASTR1 - NASA RMC 2017"
+  t.window.title = "Navigation Visualizer - UNCC_ASTR1 - NASA RMC 2017"
   t.window.icon = "icon.png"
   t.window.height = 512
   t.window.width = 1000
